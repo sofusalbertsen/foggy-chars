b7c0818266b27d89a5e3c96691bd17721a1d86c4
Sofus Albertsen <sofusalbertsen@gmail.com>
2022-02-10 21:57:24 +0100

    #1 added stat and freetext

---
+++ src/components/Freetext.vue
@@ -0,0 +1,23 @@
+<template>
+              <div class="col-sm-6">
+                <h1>{{ text }}</h1>
+              <label for="firstName" class="form-label"> {{ fieldname }} </label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+</template>
+
+<script>
+export default {
+  props: ['fieldname', 'text'],
+  created () {
+    console.log(this.text)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Stat.vue
@@ -0,0 +1,16 @@
+<template>
+            <div class="col-md-2">
+              <label for="{{statname}}" class="form-label"> {{statname}} </label>
+              <input type="number" class="form-control" id="{{statname}}" placeholder="" min="0" max="30">
+            </div>
+</template>
+
+<script>
+export default {
+  props: ['statname', 'number', 'group']
+}
+</script>
+
+<style>
+
+</style>
